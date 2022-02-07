a793bfa514d97ad5833ae698f4dba46e772b2b74
timothyshores <timothymshores@gmail.com>
2022-02-07 14:54:26 -0700

    Add GeckoDriver for Firebox browser and test in Firebox browser

--- src/introduction/SeleniumIntroduction.java
+++ src/introduction/SeleniumIntroduction.java
@@ -2,13 +2,18 @@ package introduction;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class SeleniumIntroduction {
 
 	public static void main(String[] args) {
 		System.setProperty("webdriver.chrome.driver", "/Users/tim/Documents/Code/Selenium-WebDriver-with-Java--Basics-to-Advanced-Frameworks/chromedriver");
+		System.setProperty("webdriver.gecko.driver", "/Users/tim/Documents/Code/Selenium-WebDriver-with-Java--Basics-to-Advanced-Frameworks/geckodriver");
+
 		
-		WebDriver driver = new ChromeDriver();
+//		WebDriver driver = new ChromeDriver();
+		WebDriver driver = new FirefoxDriver();
+		
 		driver.get("https://google.com");
 		System.out.println(driver.getTitle());
 		System.out.println(driver.getCurrentUrl());
