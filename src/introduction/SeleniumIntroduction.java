01ec69e87e0874c7a6785144740ee47e6a3bd3e5
Tim <tim>
2022-02-07 12:10:55 -0700

    Add chromedriver and set chromedriver location

--- src/introduction/SeleniumIntroduction.java
+++ src/introduction/SeleniumIntroduction.java
@@ -1,10 +1,14 @@
 package introduction;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class SeleniumIntroduction {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.setProperty("webdriver.chrome.driver", "/Users/tim/Documents/Code/Selenium-WebDriver-with-Java--Basics-to-Advanced-Frameworks/chromedriver");
+		
+		WebDriver driver = new ChromeDriver();
+		
 	}
-
 }
