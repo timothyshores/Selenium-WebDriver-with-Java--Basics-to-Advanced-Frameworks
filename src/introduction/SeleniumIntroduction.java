2617ad2109f6af8d52f7ad2227fb8a4586e09172
timothyshores <timothymshores@gmail.com>
2022-02-07 18:06:55 -0700

    Add Microsoft Edge browser driver and test application locally

--- src/introduction/SeleniumIntroduction.java
+++ src/introduction/SeleniumIntroduction.java
@@ -1,18 +1,20 @@
 package introduction;
 
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+//import org.openqa.selenium.chrome.ChromeDriver;
+//import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class SeleniumIntroduction {
 
 	public static void main(String[] args) {
 		System.setProperty("webdriver.chrome.driver", "/Users/tim/Documents/Code/Selenium-WebDriver-with-Java--Basics-to-Advanced-Frameworks/chromedriver");
 		System.setProperty("webdriver.gecko.driver", "/Users/tim/Documents/Code/Selenium-WebDriver-with-Java--Basics-to-Advanced-Frameworks/geckodriver");
-
+		System.setProperty("webdriver.edge.driver", "/Users/tim/Documents/Code/Selenium-WebDriver-with-Java--Basics-to-Advanced-Frameworks/edgedriver");
 		
 //		WebDriver driver = new ChromeDriver();
-		WebDriver driver = new FirefoxDriver();
+//		WebDriver driver = new FirefoxDriver();
+		WebDriver driver = new EdgeDriver();
 		
 		driver.get("https://google.com");
 		System.out.println(driver.getTitle());
