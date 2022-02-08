712e35ead26ad79ed094216880edf328adb5bc2a
timothyshores <timothymshores@gmail.com>
2022-02-07 18:29:31 -0700

    Print variables

--- src/introduction/CoreJavaConcepts.java
+++ src/introduction/CoreJavaConcepts.java
@@ -8,5 +8,11 @@ public class CoreJavaConcepts {
 		String name = "Tim";
 		double price = 9.99;
 		boolean userIsActive = true;
+		
+		System.out.println("num: " + num);
+		System.out.println("letter: " + letter);
+		System.out.println("name: " + name);
+		System.out.println("price: " + price);
+		System.out.println("userIsActive: " + userIsActive);
 	}
 }
