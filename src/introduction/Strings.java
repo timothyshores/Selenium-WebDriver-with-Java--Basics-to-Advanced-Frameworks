c8470fdfe84ae8d521c7246461e64e2ddcfd1b82
timothyshores <timothymshores@gmail.com>
2022-02-09 14:06:23 -0700

    Create Strings class and add string literals and string objects

---
+++ src/introduction/Strings.java
@@ -0,0 +1,15 @@
+package introduction;
+
+public class Strings {
+
+	public static void main(String[] args) {
+		// String Literal
+		String str1 = "Tim";
+		String str2 = "Tim";
+		
+		// String Object
+		String str3 = new String("Tim");
+		String str4 = new String("Tim");
+	}
+
+}
