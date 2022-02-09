bc1af62eba5106100ea2d42c1a6af7f603fc2ab6
timothyshores <timothymshores@gmail.com>
2022-02-09 14:51:46 -0700

    Move getData() and writeThatDown() methods to MethodsClass java class

---
+++ src/introduction/MethodsClass.java
@@ -0,0 +1,12 @@
+package introduction;
+
+public class MethodsClass {
+	public void getData() {
+		System.out.println("This is your data 💩");
+	}
+	
+	public void writeThatDown() {
+		System.out.println("Noted ✍️");
+	}
+
+}
