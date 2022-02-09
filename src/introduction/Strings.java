dfc9051d3136311444f9266e33b068a5b40a2a07
timothyshores <timothymshores@gmail.com>
2022-02-09 14:13:59 -0700

    Use built in String method .split and store result in array of strings

--- src/introduction/Strings.java
+++ src/introduction/Strings.java
@@ -10,6 +10,16 @@ public class Strings {
 		// String Object
 		String str3 = new String("Tim");
 		String str4 = new String("Tim");
+		
+		// Split a given string by whitespaces " "
+		// E.g. "Selenium WebDriver with Java" -> ["Selenium", "WebDriver", "with", Java"]
+		
+		String courseName = "Selenium WebDriver with Java";
+		String[] courseNameArray = courseName.split(" ");
+		System.out.println(courseNameArray[0]);
+		System.out.println(courseNameArray[1]);
+		System.out.println(courseNameArray[2]);
+		System.out.println(courseNameArray[3]);
 	}
 
 }
