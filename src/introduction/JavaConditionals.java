670f44764a3289f595f0bc394307c77a4f1c16a1
timothyshores <timothymshores@gmail.com>
2022-02-09 09:10:04 -0700

    Add if conditional problem and solution

--- src/introduction/JavaConditionals.java
+++ src/introduction/JavaConditionals.java
@@ -5,12 +5,19 @@ public class JavaConditionals {
 	public static void main(String[] args) {
 		int[] nums = {1,2,3,4,5};
 		
-		// TODO: 
-		// Multiply each number in the nums array and return each element multiplied by 2
-		// E.g. {1, 2, 3, 4} -> {2, 4, 6, 8}
+		System.out.println("Multiply each number in the nums array and return each element multiplied by 2");
+		System.out.println("E.g. {1, 2, 3, 4} -> {2, 4, 6, 8}");
 		
 		for (int num: nums) {
 			System.out.println(num * 2);
+		}
+		
+		System.out.println("\nPrint all odd nums in the nums array. E.g {1, 2, 3} -> {1, 3}");
+		
+		for (int num: nums) {
+			if (num % 2 == 1) {
+				System.out.println(num);
+			}
 		}
 	}
 
