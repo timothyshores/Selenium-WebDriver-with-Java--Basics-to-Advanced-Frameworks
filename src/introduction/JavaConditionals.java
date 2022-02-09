a57ec55b4a3bfe2cd615aac4e0b85d318d69f8c2
timothyshores <timothymshores@gmail.com>
2022-02-09 09:04:12 -0700

    Create new class for conditionals

---
+++ src/introduction/JavaConditionals.java
@@ -0,0 +1,13 @@
+package introduction;
+
+public class JavaConditionals {
+
+	public static void main(String[] args) {
+		int[] nums = {1,2,3,4,5};
+		
+		// TODO: 
+		// Multiply each number in the nums array and return each element multiplied by 2
+		// E.g. {1, 2, 3, 4} -> {2, 4, 6, 8}
+	}
+
+}
