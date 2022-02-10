d794f3e3e194e329b3d1c6827252a0054be6e15d
timothyshores <timothymshores@gmail.com>
2022-02-09 19:07:23 -0700

    Add implicit wait

--- src/introduction/Locators.java
+++ src/introduction/Locators.java
@@ -1,5 +1,7 @@
 package introduction;
 
+import java.time.Duration;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -8,7 +10,9 @@ public class Locators {
 
 	public static void main(String[] args) {
 		System.setProperty("webdriver.chrome.driver", "/Users/tim/Documents/Code/Selenium-WebDriver-with-Java--Basics-to-Advanced-Frameworks/chromedriver");
+		
 		WebDriver driver = new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
 		
 		driver.get("https://rahulshettyacademy.com/locatorspractice/");
 		driver.findElement(By.id("inputUsername")).sendKeys("tim");
